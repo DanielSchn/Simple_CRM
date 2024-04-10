f097f9500b876ccd3a4a910ec18b64ac89e6cb9c
DanielSchn <d.schneider@dsc-mail.de>
2024-04-10 16:39:06 +0200

    saveUser function and user model

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -5,6 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { User } from '../../models/user.class';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -15,11 +17,21 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatButtonModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    FormsModule
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+  user = new User();
+  birthDate: Date = new Date();
 
+
+
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log(this.user);
+    
+  }
 }
