44f3aba21ff612e608c5220a6df71f051cd923f5
DanielSchn <d.schneider@dsc-mail.de>
2024-04-15 14:33:55 +0200

    user detail card

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, inject, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -6,11 +6,10 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
-import { Firestore, collection, collectionData, onSnapshot, query, limit } from '@angular/fire/firestore';
-import { Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { User } from '../../models/user.class';
 import { UserService } from '../firebase-services/user.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -22,7 +21,8 @@ import { UserService } from '../firebase-services/user.service';
     MatDialogModule,
     MatNativeDateModule,
     MatCardModule,
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
