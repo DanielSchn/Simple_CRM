80f3567124902157a8ff5f1de3cb0d3b665026ba
DanielSchn <d.schneider@dsc-mail.de>
2024-04-15 15:35:18 +0200

    show user detaildata from firestore

--- src/app/firebase-services/user.service.ts
+++ src/app/firebase-services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { User } from '../../models/user.class';
-import { Firestore, collection, onSnapshot, query, limit } from '@angular/fire/firestore';
+import { Firestore, collection, onSnapshot, query, limit, doc, getDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 
@@ -38,6 +38,18 @@ export class UserService {
     getUserRef() {
         return collection(this.firestore, 'users');
     }
+
+
+    async getSingleDocRef(docId: string) {
+        const docRef = doc(collection(this.firestore, 'users'), docId);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            return docSnap.data();
+        } else {
+            console.log('No Data to load');
+            return null;
+        }
+      }
 
 
     ngOnDestroy() {
--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../firebase-services/user.service';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -11,5 +14,20 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './user-detail.component.scss'
 })
 export class UserDetailComponent {
+  id: string = '';
+  userData: User = new User();
 
+  constructor(private route: ActivatedRoute, private userService: UserService) {
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+    this.getSingleUser(this.id);
+  }
+
+  getSingleUser(id: string) {
+    this.userService.getSingleDocRef(id).then(data => {
+      this.userData = new User(data);
+      console.log(this.userData);
+    }).catch(error => {
+      console.error('Error fetching User', error);
+    });
+  }
 }
